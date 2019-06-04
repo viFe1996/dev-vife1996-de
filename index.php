9bd8a90ec9c043bf86c7b873120f994434f2c42c
viFe1996 <32300789+viFe1996@users.noreply.github.com>
2019-06-04 13:25:02 +0200

    generateRandomString function generated

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<p>Test</p><p>test2</p>
+<p>Test</p>
+<?php
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+echo "<br>";
+echo generateRandomString();
+echo "<br>";
